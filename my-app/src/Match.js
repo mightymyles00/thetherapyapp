d16243b35bea8ee14e5f87307c3c293e3ffb61cb
mightymyles00 <mightymyles00@gmail.com>
2024-09-20 23:45:06 -0700

    images and match

---
+++ my-app/src/Match.js
@@ -0,0 +1,31 @@
+import './App.css';
+import welcomeImage from './welcome.png';
+import startButton from './start.png';
+import activitiesButton from './activities.png';
+import statsButton from './stats.png'
+import petButton from './pet.png'
+
+
+
+import Card from 'react-bootstrap/Card'
+//import CardBody from 'react-bootstrap/CardBody'
+//import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+
+
+function Match() {
+  return (
+    <><Container className='container'>
+
+
+      </Container>
+      
+  </>
+  );
+};
+
+
+export default Match;
+
+
