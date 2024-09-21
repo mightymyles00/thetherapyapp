250c34f33370fa6ab1bbc6a86b04ce34c1705fbd
mightymyles00 <mightymyles00@gmail.com>
2024-09-20 21:49:23 -0700

    made the home screen with the buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,71 @@
 import './App.css';
 import welcomeImage from './welcome.png';
+import startButton from './start.png';
+import activitiesButton from './activities.png';
+import statsButton from './stats.png'
+import petButton from './pet.png'
+
+
+
 import Card from 'react-bootstrap/Card'
 //import CardBody from 'react-bootstrap/CardBody'
-import Button from 'react-bootstrap/Button'
+//import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 
 
 
 function App() {
   return (
-    <Container>
+    <><Container className='container'>
+      <table>
+        <tr>
+      <Card className='WelcomeMessage'>
+        <Card.Body>
+          <table>
+            <tr>
+              <td>
+                <img src={welcomeImage} alt='meo' height='200px' style={{ borderRadius: "25px" }} />
+              </td>
+              <td>
+                <p>Good Morning, Myles</p>
+                <p>Came back for more brain exercise? </p>
+              </td>
+            </tr>
+          </table>
+        </Card.Body>
 
-        <Card id='welcome'>        
-            <Card.Body> Hello </Card.Body>
-            <p> hello </p>
+      </Card>
+      </tr>
+      <br></br>
+      <tr>
+        <Card className='WelcomeMessage'>
+          <table border={4}>
+            <tr aria-rowspan={2}>
+              <td colSpan={2}>
+                <button> <img src={startButton} alt='start' height='110'/> </button>
+              </td>
+            </tr>
+            <tr>
+              
+              <td width={'50%'}>
+                <button> <img src={activitiesButton} alt='activities' height={175}/> </button> 
+              </td>
+              <tr>
+                <button> <img src={statsButton} alt='stats' height={100}/> </button>
 
+                <button> <img src={petButton} alt='stats' height={75}/> </button>
+              </tr>
+              
+            </tr>
+            
+          </table>
         </Card>
-      </Container>
+        </tr>
+      </table>
 
+      </Container>
+      
+  </>
   );
 };
 
