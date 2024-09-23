893b28b9702b0253a90d3d4b257d2df92ca9659a
mightymyles00 <mightymyles00@gmail.com>
2024-09-23 16:42:00 -0700

    simon memory game implemeneted (all it needs is sound effects)

--- my-app/src/Simon.js
+++ my-app/src/Simon.js
@@ -10,6 +10,7 @@ const colors = ["green", "red", "yellow", "blue"]
 function Simon() {
   const [sequence, setSequence] = useState([]);
   const [playing, setPlaying] = useState(false);
+  const [playingIdx, setPlayingIdx] = useState(0);
 
   //reference buttons
   const greenRef = useRef(null);
@@ -18,7 +19,13 @@ function Simon() {
   const blueRef = useRef(null);
 
 
-  //function
+  //function 
+  const resetGame = () => {
+    setSequence([])
+    setPlaying(false)
+    setPlayingIdx(0)
+  }
+
   const addNewColor = () => {
     const color = colors[Math.floor(Math.random() * 4)];
     const newSequence = [...sequence, color];
@@ -31,6 +38,45 @@ function Simon() {
       setPlaying(true);
       addNewColor();
     }
+  }
+
+  const handleColorClick = (e) => {
+    if (playing) {
+      e.target.classList.add("opacity-50");
+
+      setTimeout(() => {
+        e.target.classList.remove("opacity-50")
+
+      })
+
+      const clickColor = e.target.getAttribute("color");
+
+      // click the correct color of the sequence
+      if(sequence[playingIdx] === clickColor) // CORRECT 
+      {
+        if (playingIdx === sequence.length -1) {
+          setTimeout(() => {
+            setPlayingIdx(0);
+            addNewColor();
+          }, 250);
+
+
+        } 
+        else {
+          setPlayingIdx(playingIdx + 1);
+        }
+        //missing colors of the seuqenece to be clicked
+
+
+      }
+      else // INCORRECT
+      {
+        
+        resetGame()
+        alert("You Lost!");
+      }       
+    }
+
   }
 
   // excutes if the sequence changes
@@ -48,14 +94,15 @@ function Simon() {
         
         console.log(sequence[idx])
         setTimeout(() => {
-          //var xref = document.getElementById(sequence[idx])
+          var xref = document.getElementById(sequence[idx])
           //xref.current.classList.add("bright-buttons")
-          console.log(ref) 
-          ref.current.classList.add("brightness-[2.5]");
+          //console.log(ref) 
+          //xref.style.backgroundColor = "white"
+          ref.current.classList.add("bright-buttons");
 
           setTimeout(() => {
             //xref.current.classList.remove("bright-buttons")
-            ref.current.classList.remove("brightness-[2.5]");
+            ref.current.classList.remove("bright-buttons");
             if (idx < sequence.length -1) showSequence(idx + 1);
           }, 250);
         }, 250);
@@ -83,7 +130,7 @@ function Simon() {
         <tr>
           <table>
 
-      <button style={{height: "40px", alignContent: 'center'}} onClick={handleNextLevel}> Start </button>
+      <button style={{height: "40px", alignContent: 'center'}} onClick={handleNextLevel}> {sequence.length === 0 ? "Start" : sequence.length} </button>
       </table>
       </tr>
       <br></br>
@@ -94,18 +141,18 @@ function Simon() {
         <table>
           <tr>
             <td>
-              <SimonButton id="green" bg="green" border="200px 0px 0px 0px" ref={greenRef}/>
+              <SimonButton color="green" bg="green" border="200px 0px 0px 0px" onClick={handleColorClick} ref={greenRef}/>
             </td>
             <td>
-              <SimonButton id="red" bg="red" border="0px 200px 0px 0px" ref={redRef}/> 
+              <SimonButton color="red" bg="red" border="0px 200px 0px 0px" onClick={handleColorClick} ref={redRef}/> 
             </td>
           </tr>
           <tr>
             <td>
-              <SimonButton id="yellow" bg="yellow" border="0px 0px 0px 200px" ref={yellowRef}/>
+              <SimonButton color="yellow" bg="yellow" border="0px 0px 0px 200px" onClick={handleColorClick} ref={yellowRef}/>
             </td>
             <td>
-              <SimonButton id="blue" bg="blue" border="00px 0px 200px 0px" ref={blueRef}/>
+              <SimonButton color="blue" bg="blue" border="00px 0px 200px 0px" onClick={handleColorClick} ref={blueRef}/>
             </td>
           </tr>
           </table>
--- my-app/src/components(simon)/SimonButton.js
+++ my-app/src/components(simon)/SimonButton.js
@@ -6,30 +6,9 @@ import React, { forwardRef } from 'react'
 
 
 
-const GameBtn = forwardRef(( {border, bg, onClick}, ref) => (
-    /*var roundLocation
-
-    if (border === 'top-left') 
-    {
-        roundLocation = "200px 0px 0px 0px"
-    }
-    if (border === 'top-right') 
-    {
-        roundLocation = "0px 200px 0px 0px"
-    }
-    if (border === 'bottom-left') 
-    {
-        roundLocation = "0px 0px 200px 0px"
-    }
-    if (border === 'bottom-right') 
-    {
-        roundLocation = "0px 0px 0px 200px"
-    }
-    
-    else {
-        return;
-    }*/
+const GameBtn = forwardRef(( { color, border, bg, onClick}, ref) => (   
     <button 
+        color={color}
         style={{background: bg, borderRadius: border}} 
         onClick={onClick}
         ref={ref}
