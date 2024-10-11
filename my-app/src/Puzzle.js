9d29d6ff9c07e086cf785066bfe0233ce901242d
mightymyles00 <mightymyles00@gmail.com>
2024-10-11 12:29:49 -0700

    puzzle page can now verify if the puzzle is complete.  Trying to make the alert box "Puzzle finished" to appear when its finished.  Added assets for react page

--- my-app/src/Puzzle.js
+++ my-app/src/Puzzle.js
@@ -17,11 +17,10 @@ import Container from 'react-bootstrap/Container'
 
 const Puzzle = () => {
     const [positions, setPositions] = useState([...Array(16).keys()]);
-    //correctPosition = use
     const [solved, setSolved] = useState(false);
-    const [pieces, setPieces] = useState([
-        { id: 1, position: { left: 50, top: 50 }, correctPosition: { left: 100, top: 100 } },
-    ])
+    const [correct, setCorrect] = useState([...Array(16).keys()])
+    const [started, setStarted] = useState(false);
+    const [firstPass, setFirstPass] = useState(false)
 
     // Handling the start of a drag event
     const handleDragStart = (e,position) => {
@@ -32,7 +31,7 @@ const Puzzle = () => {
     const handleDrop = (e, position) => {
         e.preventDefault();
         const originalPosition = e.dataTransfer.getData('text');
-
+        console.log(originalPosition)
         setPositions((prevPositions) => {
             const newPos = [...prevPositions];
             [newPos[originalPosition], newPos[position]] = [
@@ -40,8 +39,12 @@ const Puzzle = () => {
                 newPos[originalPosition]
                 
             ];
+
             return newPos
         });
+
+
+
         
     };
 
@@ -49,31 +52,53 @@ const Puzzle = () => {
         e.preventDefault();
     }
 
-    const isSolved = (id) => {
-        const solved = false
-        for (let i = 0; i < 16; i++)
-        {
-            //solved && (positions.indexOf(i) === parseInt(document.getElementById(3)))   
-        }
-        //positions.findIndex
+    const handleStart = (e) => {
+        e.preventDefault();
+        if(started === false) {
+            setStarted(true)
 
-
-        
-        
     }
+
+
+
+    }
+
+    function isSolved() {
+        setSolved(JSON.stringify(positions) === JSON.stringify(correct))
+        console.log(positions)
+        console.log(solved)
+        if (solved && started)
+        {
+            alert("Puzzle finished");
+        }
+
     
+    }
 
 
+    // Activates at the start of the game
     useEffect(() => {
-         
         //Shuffle the positions
-        setPositions((prevPositions) => {
-            const newPos = [...prevPositions];
-            newPos.sort(() => Math.random() - 0.5);
-            return newPos;
-        });
+        if(started === true)
+        {
+            setPositions((prevPositions) => {
+                const newPos = [...prevPositions];
+                newPos.sort(() => Math.random() - 0.5);
+                return newPos;
+                });
+        }
+    }, [started]);
 
-    }, []);
+    // Checks if the puzzle is completed        
+    useEffect((e) => {
+        if(started === true)
+        {
+            isSolved()
+        }
+
+    }, [positions]);
+
+
     return (
     <>
     <Container className='container'>
@@ -87,7 +112,7 @@ const Puzzle = () => {
         </tr>
         <tr>
     <table>
-        <button style={{height: "40px", alignContent: 'center'}}> "Start" </button>
+        <button style={{height: "40px", alignContent: 'center'}} onClick={handleStart}> "Start" </button>
     </table>
       </tr>
       <br></br>
@@ -115,7 +140,7 @@ const Puzzle = () => {
                         backgroundImage: `url('${ReferenceImage}')`,
                         backgroundPosition: `-${x}px -${y}px`
                     }}
-                > {pos}</div>
+                ></div>
                 );
             })
             }
